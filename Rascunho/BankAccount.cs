b795606d5ba73e5844056acc18915b7ba34395e4
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-12 17:20:33 -0300

    Exercise Finished

---
+++ Rascunho/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class BankAccount
+    {
+        private double _rate = 5.00;
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public BankAccount(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = 0.0;
+        }
+
+        public BankAccount(int number, string holder, double balance) : this(number, holder)
+        {
+            Deposit(balance);
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount + _rate)
+            {
+                Balance -= amount + _rate;
+            }
+
+            else if (amount <= Balance && Balance <= amount + _rate)
+            {
+                Console.WriteLine("You do not have a balance to withdraw this amount  and cover the fee.");
+            }
+
+            else
+            {
+                Console.WriteLine("You do not have sufficient balance to make the withdrawal.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Account: {Number}, Holder: {Holder}, Banck balance: {Balance.ToString()}";
+        }
+    }
+}
---
+++ Rascunho/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount;
+            double amount;
+            Console.Write("Enter the account number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the holder account:");
+            string hoder = Console.ReadLine();
+            Console.Write("Is there an initial deposit (S/N)? ");
+            char isThereDeposit = char.Parse(Console.ReadLine().ToUpper());
+            if(isThereDeposit == 'S')
+            {
+                Console.Write("Enter the amount to initial deposit: ");
+                amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                bankAccount = new BankAccount(number, hoder, amount);
+            }
+            else
+            {
+                bankAccount = new BankAccount(number, hoder);
+            }
+            Console.WriteLine("Account data:");
+            Console.WriteLine(bankAccount);
+            Console.Write("Enter the amount to deposit: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            bankAccount.Deposit(amount);
+            Console.WriteLine(bankAccount);
+            Console.Write("Enter the amount to withdraw: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            bankAccount.Withdraw(amount);
+            Console.WriteLine(bankAccount);
+
+            Console.ReadKey();
+        }      
+
+    }
+}
